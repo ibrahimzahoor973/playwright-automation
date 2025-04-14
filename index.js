1e20c6c2202e25614aa17bd3f21d5bf2e1845fd8
Ibrahim Zahoor <ibrahimzahoor973@gmail.com>
2025-04-14 16:49:58 +0500

    index file updated

--- index.js
+++ index.js
@@ -4,42 +4,10 @@ import { PLATFORMS } from './constants.js';
 
 const { platform, uploadScript = false } = process.env;
 
-if (uploadScript) {
-    import('./src/upload-galleries.js')
-    .then(() => {
-        console.log('Upload Galleries Module Loaded');
-    })
-    .catch(err => {
-        console.error('Error loading the module:', err);
-    });
-} else if (platform === PLATFORMS.PIXIESET) {
+if (platform === PLATFORMS.PIXIESET) {
     import('./controllers/pixie-set/index.js')
     .then(() => {
         console.log('Pixieset Module Loaded');
-    })
-    .catch(err => {
-        console.error('Error loading the module:', err);
-    });
-} else if (platform === PLATFORMS.PIC_TIME) {
-    import('./src/pages/pic-time.js')
-    .then(() => {
-        console.log('Pic Time Module Loaded');
-    })
-    .catch(err => {
-        console.error('Error loading the module:', err);
-    });
-} else if (platform === PLATFORMS.SHOOTPROOF) {
-    import('./src/pages/shootproof.js')
-    .then(() => {
-        console.log('Shoot Proof Module Loaded');
-    })
-    .catch(err => {
-        console.error('Error loading the module:', err);
-    });
-} else if (platform === PLATFORMS.ZENFOLIO) {
-    import('./src/pages/zenfolio.js')
-    .then(() => {
-        console.log('ZenFolio Module Loaded');
     })
     .catch(err => {
         console.error('Error loading the module:', err);
